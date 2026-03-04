0f9a814bf100d9b43998faa3171f799fe3f941fa
luann <luanndebortoli@gmail.com>
2026-03-03 21:11:33 -0300

    feat: base request save

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,37 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import axios from 'axios'
 
 export default function Home() {
+
+  const [ollamaRes, setOllamaRes] = useState("")
+
+  const postRequest = async () => {
+  try {
+    const res = await axios.post(
+      "http://localhost:11434/api/generate",
+      {
+        model: "gemma3:1b",
+        prompt: "hey, how are you? answer in 1 word",
+        stream: false
+      }
+    )
+
+    setOllamaRes(res.data.response)
+    console.log(res.data.response)
+
+  } catch (err: any) {
+    console.log(err.response?.status, err.response?.data)
+  }
+}
+
+  useEffect(() => {
+    postRequest()
+  },[])
+
+
   return (
     <>
     </>
