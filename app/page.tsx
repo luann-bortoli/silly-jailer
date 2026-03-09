c5539a217831ee61c759dc30f14bd08c89ebd2b3
luann-bortoli <luanndebortoli@gmail.com>
2026-03-09 18:13:15 -0300

    feat: interface template

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import styles from "./page.module.css";
 import axios from 'axios'
 
@@ -9,31 +9,50 @@ export default function Home() {
   const [ollamaRes, setOllamaRes] = useState("")
 
   const postRequest = async () => {
-  try {
-    const res = await axios.post(
-      "http://localhost:11434/api/generate",
-      {
-        model: "gemma3:1b",
-        prompt: "hey, how are you? answer in 1 word",
-        stream: false
-      }
-    )
+    try
+    {
+      const res = await axios.post(
+        "http://localhost:11434/api/generate",
+        {
+          model: "gemma3:1b",
+          prompt: "hey, how are you? answer in 1 word",
+          stream: false
+        }
+      )
 
-    setOllamaRes(res.data.response)
-    console.log(res.data.response)
+      setOllamaRes(res.data.response)
+      console.log(res.data.response)
 
-  } catch (err: any) {
-    console.log(err.response?.status, err.response?.data)
+    } catch (err: any)
+    {
+      console.log(err.response?.status, err.response?.data)
+    }
   }
-}
 
   useEffect(() => {
     postRequest()
-  },[])
+  }, [])
 
+  const inputRef = useRef<HTMLTextAreaElement | null>(null)
+
+  function keepInputFocus() {
+    inputRef.current?.focus()
+  }
 
   return (
     <>
+      <div className={styles.container}>
+        <div className={styles.terminalContainer}>
+
+          <div className={styles.responseContainer}>
+          </div>
+
+          <div className={styles.inputContainer}>
+            <textarea className={styles.requestInput} rows={1} ref={inputRef} autoFocus onBlur={keepInputFocus} />
+          </div>
+
+        </div>
+      </div>
     </>
   );
 }
